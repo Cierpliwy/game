d0c4bb3cc31090f0dbeb65e8bef082bb91f6a2d0
Przemysław Lenart <przemek.lenart@gmail.com>
2014-01-18 16:49:14 +0100

    Dodano detekcje czy linia jest podłogą

--- src/Utils.h
+++ src/Utils.h
@@ -4,8 +4,9 @@
 
 template<class T> struct Line
 {
-    Line(T a, T b) : a(a), b(b) {}
+    Line(T a, T b, bool floor) : a(a), b(b), floor(floor) {}
     T a,b;
+    bool floor;
 };
 
 template<class T> struct Rect
